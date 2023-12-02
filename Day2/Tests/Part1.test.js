6199a3a83310c24820593a780cf41fdc1d8bc3a9
Jean Jeannie <jmcclymont@gmail.com>
2023-12-02 16:15:17 +1300

    Add initial files for Day 2
    Also added HelloWorld method and test to templates

--- Day2/Tests/Part1.test.js
+++ Day2/Tests/Part1.test.js
@@ -1,11 +1,11 @@
-//const day3 = require('../Part1');
+const day = require('../Part1');
 
 describe("example test", () => {
   test("it is true", () => { 
     expect(true).toBe(true);
   })
   test("it is false", () => { 
-    expect(false).toBe(true);
+    expect(false).toBe(false);
   })
 })
 
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { GetResult } from "./Day1/Part2.js";
+import { GetResult } from "./Day2/Part1.js";
 import * as url from 'url';
 //const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-let day = "Day1";
-let sampleFile = "SampleInputPart2.txt";
+let day = "Day2";
+let sampleFile = "SampleInput.txt";
 let puzzleInput = "Input.txt"
 let inputfile = puzzleInput;
 let logOutput = false;
