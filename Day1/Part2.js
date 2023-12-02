573393637ddf6344784d66c85fb1ba5cb750a1b6
Jean Jeannie <jmcclymont@gmail.com>
2023-12-02 14:57:00 +1300

    Extracts first number successfully
    Including if it's a numeric literal like "one"

--- Day1/Part2.js
+++ Day1/Part2.js
@@ -13,6 +13,106 @@ export function IsNumber(input) {
     return !isNaN(input);
 }
 
+const textToNumber = {
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9,
+        zero: 0
+    }
+
+export function LiteralNumbers() {
+    let keys = Object.keys(textToNumber);
+    return keys;
+} 
+
+export function LiteralNumbersAsArray() {
+    return LiteralNumbers().map( (element) => element.split(''));
+}
+
+export function LastLetterOfLiteralNumbers() {
+    return LiteralNumbersAsArray().map( (element) => { 
+        return element[element.length-1];
+    });
+}
+
+export function FirstLetterOfLiteralNumbers() {
+    return LiteralNumbersAsArray().map( (element) => { 
+        return element[0];
+    });
+}
+
+export function FindPotentialLiterals(input) {
+    return LiteralNumbers().filter( (element) => element.split('')[0] === input);
+}
+
+export function CheckAheadIfLiteralNumber(input) {
+// read each char.  If it's a number return it, if it's the first character of a literal number
+// check to see if the following characters match.  
+    let array = input.split('');
+    for (let index = 0; index < array.length; index++) { 
+        let curChar = array[index];
+        if (IsNumber(curChar)) {
+            return parseInt(curChar, 10);
+        }
+        let potentialLiteral = FirstLetterOfLiteralNumbers().findIndex((el) => el === curChar);
+        if (potentialLiteral >= 0){
+            return potentialLiteral;
+        }
+    }
+}
+
+export function FindFirstMatchingNumber(input){
+    let array = input.split('');
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (IsNumber(element)) {
+            return parseInt(element, 10);
+        }
+
+        let remainingArray = array;
+        if (index !== 0){
+            remainingArray = array.slice(index);
+        }
+        let matchingLiteral = FindMatchingLiteral(remainingArray);
+        if (matchingLiteral != null)
+        {
+            return matchingLiteral;
+        }        
+    }
+    return null;
+}
+
+
+export function FindMatchingLiteral(input){
+//    let inputArray = input.split('');
+    let potentialLiteral = FindPotentialLiterals(input[0]);
+   // return potentialLiteral;
+    var inputNumber = null;
+
+    for (let index = 0; index < potentialLiteral.length; index++) {
+        let element = potentialLiteral[index];
+        let lenElement = element.length;
+        let potentialInput = input.slice(0, lenElement).join('');
+        if (element == potentialInput){
+            inputNumber = textToNumber[potentialInput];
+            break;
+        }        
+    }
+    
+    return inputNumber;
+}
+
+export function ParseTextAsNumber(input){
+    var num = textToNumber[input];
+    return num ?? null;
+}
+
 export function ParseNumbers(input) {
     var chars = input.split('');
     var numbers = chars.filter(IsNumber)
--- Day1/Tests/Part2.test.js
+++ Day1/Tests/Part2.test.js
@@ -11,6 +11,143 @@ describe("Is Number", () => {
   })
 });
 
+describe("Read literal numbers", () => {
+  test("it returns 1 when input is one", () => {
+    let res = day.ParseTextAsNumber("one");
+    expect(res).toBe(1);
+  }),
+  test("it returns 7 when input is seven", () => {
+    let res = day.ParseTextAsNumber("seven");
+    expect(res).toBe(7);
+  }),
+  test("it returns null when input is not a number", () => {
+    let res = day.ParseTextAsNumber("onne");
+    expect(res).toBeNull();
+  })
+})
+
+describe("Get list of literal numbers", () => {
+  test("returns array of literal numbers", () => {
+    let res = day.LiteralNumbers();
+    expect(res).toEqual(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']);
+  })
+})
+
+describe("Get literal numbers as arrays", () => {
+  test("returns o n e from first element", () => {
+    let res = day.LiteralNumbersAsArray();
+    expect(res[0]).toEqual(['o','n','e']);
+  }), 
+  test("returns s i x from sixth element", () => {
+    let res = day.LiteralNumbersAsArray();
+    expect(res[5]).toEqual(['s','i','x']);
+  })
+})
+
+describe("Get last character of literal numbers", () => {
+  test("returns e from first element", () => {
+    let res = day.LastLetterOfLiteralNumbers();
+    expect(res[0]).toBe('e');
+  }), 
+  test("returns n from seventh element", () => {
+    let res = day.LastLetterOfLiteralNumbers();
+    expect(res[6]).toBe('n');
+  }),
+  test("returns eoerexnteo as the last numbers of the literal numbers", () => {
+    let res = day.LastLetterOfLiteralNumbers();
+    expect(res).toEqual(['e','o','e','r','e','x','n','t','e','o']);
+  })
+})
+
+describe("Get first character of literal numbers", () => {
+  test("returns o from first element", () => {
+    let res = day.FirstLetterOfLiteralNumbers();
+    expect(res[0]).toBe('o');
+  }), 
+  test("returns s from seventh element", () => {
+    let res = day.FirstLetterOfLiteralNumbers();
+    expect(res[6]).toBe('s');
+  }),
+  test("returns ottffssenz as the first numbers of the literal numbers", () => {
+    let res = day.FirstLetterOfLiteralNumbers();
+    expect(res).toEqual(['o','t','t','f','f','s','s','e','n','z']);
+  })
+})
+
+/*
+describe("Can extract literal number out of a string", () => {
+  test("returns 4 from abc4def", () => {
+    let res = day.CheckAheadIfLiteralNumber("abc4def");
+    expect(res).toBe(4);
+  })
+  test("returns 6 from abcsixdef", () => {
+    let res = day.CheckAheadIfLiteralNumber("abcsixdef");
+    expect(res).toBe(6);
+  })
+})
+*/ 
+
+describe("Find potential literals starting with the letter provided", () => {
+  test("returns one when input given is o", () => {
+    let res = day.FindPotentialLiterals("o");
+    expect(res).toEqual(["one"]);
+  }), 
+  test("returns six, seven when input given is s", () => {
+    let res = day.FindPotentialLiterals("s");
+    expect(res).toEqual(["six", "seven"]);
+  }),
+  test("returns empty array when input given is d", () => {
+    let res = day.FindPotentialLiterals("d");
+    expect(res).toEqual([]);
+  })
+})
+
+describe("Find matching literal", () => {
+  test("returns 1 when input given is onebcdef", () => {
+    let res = day.FindMatchingLiteral("onebcdef".split(''));
+    expect(res).toBe(1);
+  }),
+  test("returns 3 when input given is threebcdef", () => {
+    let res = day.FindMatchingLiteral("threebcdef".split(''));
+    expect(res).toBe(3);
+  }),
+  test("returns 2 when input given is twobcdef", () => {
+    let res = day.FindMatchingLiteral("twobcdef".split(''));
+    expect(res).toBe(2);
+  }),
+  test("returns 6 when input given is sixbcdef", () => {
+    let res = day.FindMatchingLiteral("sixbcdef".split(''));
+    expect(res).toBe(6);
+  }),
+  test("returns null when input given is abcdef", () => {
+    let res = day.FindMatchingLiteral("abcdef".split(''));
+    expect(res).toBeNull();
+  })
+})
+
+describe("Find first matching number", () => {
+  test("returns 1 when input given is abconebcdef", () => {
+    let res = day.FindFirstMatchingNumber("abconebcdef");
+    expect(res).toBe(1);
+  }),
+  test("returns 3 when input given is abcthree", () => {
+    let res = day.FindFirstMatchingNumber("abcthree");
+    expect(res).toBe(3);
+  }),
+  test("returns 2 when input given is two3bcdef", () => {
+    let res = day.FindFirstMatchingNumber("two3bcdef");
+    expect(res).toBe(2);
+  }),
+  test("returns 6 when input given is se6xonebcdef", () => {
+    let res = day.FindFirstMatchingNumber("se6xonebcdef");
+    expect(res).toBe(6);
+  }),
+  test("returns null when input given is sexoebcdef", () => {
+    let res = day.FindFirstMatchingNumber("sexoebcdef");
+    expect(res).toBeNull();
+  })
+})
+
 describe("Parse Number", () => {
   test("it returns empty when no numbers", () => {
     let res = day.ParseNumbers("abc");
