83fd757098f250c4311f08da966ecb843259da69
Jean Jeannie <jmcclymont@gmail.com>
2023-12-03 22:33:18 +1300

    Initialise files for Day 4

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { GetResult } from "./Day3/Part2.js";
+import { GetResult } from "./Day4/Part1.js";
 import * as url from 'url';
 //const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-let day = "Day3";
+let day = "Day4";
 let sampleFile = "SampleInput.txt";
 let puzzleInput = "Input.txt"
-let inputfile = puzzleInput;
+let inputfile = sampleFile;
 let logOutput = true;
 
 var filename = `${__dirname}/${day}/Input/${inputfile}`;
