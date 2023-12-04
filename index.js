8b40d69b92eac0ab5c205f4bb0c5542f6fef7835
Jean Jeannie <jmcclymont@gmail.com>
2023-12-04 21:26:46 +1300

    Day 4 Part 2 sample input calculates correctly

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { GetResult } from "./Day4/Part1.js";
+import { GetResult } from "./Day4/Part2.js";
 import * as url from 'url';
 //const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -6,8 +6,8 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 let day = "Day4";
 let sampleFile = "SampleInput.txt";
 let puzzleInput = "Input.txt"
-let inputfile = puzzleInput;
-let logOutput = false;
+let inputfile = sampleFile;
+let logOutput = true;
 
 var filename = `${__dirname}/${day}/Input/${inputfile}`;
 console.log(`${day} starting`)
