511c3005e5700e73e16fcb6dc4a70ab66e36dcab
Jean Jeannie <jmcclymont@gmail.com>
2023-12-04 21:29:13 +1300

    Set up for Day 5

---
+++ Day5/Part1.js
@@ -0,0 +1,20 @@
+import { GetFileContentsAsArray } from "../Helpers/ReadFile.js";
+let DisplayLog = false;
+
+//https://adventofcode.com/2023/day/5
+
+function Log(logLevel, message){
+    if (logLevel != "INFO") console.log(`[${logLevel}] ${message}`);
+    if (logLevel == "INFO" && DisplayLog) console.log(`[${logLevel}] ${message}`);
+    return;
+}
+
+export function SayHelloWorld() {
+    return "Hello World!";
+}
+
+export function GetResult(filename, logOutput){
+    DisplayLog = logOutput;
+    var arr = GetFileContentsAsArray(filename);
+    return -1;
+}
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { GetResult } from "./Day4/Part2.js";
+import { GetResult } from "./Day5/Part1.js";
 import * as url from 'url';
 //const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-let day = "Day4";
+let day = "Day5";
 let sampleFile = "SampleInput.txt";
 let puzzleInput = "Input.txt"
-let inputfile = puzzleInput;
-let logOutput = false;
+let inputfile = sampleFile;
+let logOutput = true;
 
 var filename = `${__dirname}/${day}/Input/${inputfile}`;
 console.log(`${day} starting`)
