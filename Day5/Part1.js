e0077be0839cf49cd938d99660b46675c8ab6c3b
Jean Jeannie <jmcclymont@gmail.com>
2023-12-10 22:18:26 +1300

    Day5 Part 1 is correct

--- Day5/Part1.js
+++ Day5/Part1.js
@@ -16,6 +16,7 @@ export function ReadSeeds(input) {
     return null;
 }
 
+/** 
 export function ReadMapping(input){
     let source = [];
     let destination = [];
@@ -32,14 +33,79 @@ export function ReadMapping(input){
     }
     return { source: source, destination: destination}
 }
+**/ 
 
-export function ReadMappingFile(array){
-    
+export function ReadMapping(array, startPos){
+    startPos = startPos == undefined ? 0 : startPos;
+    let type = "";
+    let map = [];
+    let lastPos = startPos;
+    for (let index = startPos; index < array.length; index++) {
+        lastPos = index;
+        const element = array[index];
+        if (element == "") {
+            break;
+        }
 
+        let rowContents = element.split(' ');
+        if (rowContents[1] == "map:"){
+            type = rowContents[0];
+            continue;
+        }
+        let formattedRow = rowContents.map( item => parseInt(item, 10));
+        map.push( { destination: formattedRow[0] , source: formattedRow[1], length: formattedRow[2]});
+    }
+    return { result: { type: type, map: map }, lastPos: lastPos};
+}
+
+export function ReadAllMappings(array){
+    let mappings = [];
+    for (let index = 0; index < array.length; index++) {
+        let currentMapping = ReadMapping(array, index);
+        mappings.push(currentMapping.result);
+        index = currentMapping.lastPos;
+        if (currentMapping.type == "humidity-to-location"){
+            break;
+        }
+    }
+    return mappings;
+}
+
+export function GetDestinationSpot(seed, mapping){
+    for (let index = 0; index < mapping.map.length; index++) {
+        const currentMapping = mapping.map[index];
+        if (seed >= currentMapping.source 
+            && seed < currentMapping.source + currentMapping.length){
+                let diff = currentMapping.destination - currentMapping.source;
+                return seed + diff;
+        }
+    }
+    return seed;
+}
+
+export function GetLocationForSeed(seed, allMappings){
+    let destination = seed;
+    for (let index = 0; index < allMappings.length; index++) {
+        const currentMapping = allMappings[index];
+        destination = GetDestinationSpot(destination, currentMapping);
+    }
+    return destination;
 }
 
 export function GetResult(filename, logOutput){
     DisplayLog = logOutput;
     var arr = GetFileContentsAsArray(filename);
-    return -1;
+    let seeds = ReadSeeds(arr.shift());
+    let allMappings = ReadAllMappings(arr);
+    let locations = [];
+    let lowestLocation = null;
+    for (let index = 0; index < seeds.length; index++) {
+        let currentLocation = GetLocationForSeed( seeds[index], allMappings);
+        locations.push(currentLocation);
+        if (lowestLocation == null || currentLocation < lowestLocation){
+            lowestLocation = currentLocation;
+        }        
+    }
+
+    return lowestLocation;
 }
