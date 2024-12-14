fc40393131fbc56d6d160cfe77f9bb434b58c447
Jean Jeannie <jmcclymont@gmail.com>
2024-12-14 18:39:32 +1300

    Day1 Part1 successful

--- Day1/Part1.js
+++ Day1/Part1.js
@@ -1,5 +1,5 @@
 import { GetFileContentsAsArray } from "../Helpers/ReadFile.js";
-let DisplayLog = false;
+let DisplayLog = true;
 
 //https://adventofcode.com/2024/day/1
 
@@ -13,8 +13,50 @@ export function SayHelloWorld() {
     return "Hello World!";
 }
 
+export function GetLeftList(arr){
+    let left = arr;
+    return left;
+}
+
+export function SplitArray(arr){
+    let leftList = [];
+    let rightList = [];
+    arr.forEach(element => {
+        let rowArr = element.split("   ");
+        let leftNum = parseInt(rowArr[0]);
+        let rightNum = parseInt(rowArr[1]);
+        leftList.push(leftNum);
+        rightList.push(rightNum);
+    });
+    return {left: leftList, right: rightList};
+}
+
+export function SortList(arr){
+    return arr.sort();
+}
+
+export function PairDistance(leftNum, rightNum){
+    return Math.abs(leftNum-rightNum);
+}
+
+export function DistanceSums(arr){
+    let lists = SplitArray(arr);
+    let leftSort = SortList(lists.left);
+    let rightSort = SortList(lists.right);
+
+    let distanceSum = 0;
+
+    for (let index = 0; index < leftSort.length; index++) {
+        let leftNum = leftSort[index];
+        let rightNum = rightSort[index];
+        distanceSum = distanceSum + PairDistance(leftNum, rightNum);        
+    }    
+    return distanceSum;
+}
+
 export function GetResult(filename, logOutput){
     DisplayLog = logOutput;
     var arr = GetFileContentsAsArray(filename);
-    return -1;
+    //Log("INFO", arr);
+    return DistanceSums(arr);
 }
--- Day1/Tests/Part1.test.js
+++ Day1/Tests/Part1.test.js
@@ -1,17 +1,41 @@
 const day = require('../Part1');
 
-describe("example test", () => {
-  test("it is true", () => { 
-    expect(true).toBe(true);
+describe("Get left & right lists", () => {
+  test("it is an array of 6 items", () => {
+    let arrays = day.SplitArray(["3   4","4   3","2   5","1   3","3   9","3   3"]);
+    expect(arrays.left.length).toBe(6);
+  }),
+  test("the 3rd element in left array is 2", () => {
+    let arrays = day.SplitArray(["3   4","4   3","2   5","1   3","3   9","3   3"]);
+    expect(arrays.left[2]).toBe(2);
+  }),
+  test("the 3rd element in right array is 5", () => {
+    let arrays = day.SplitArray(["3   4","4   3","2   5","1   3","3   9","3   3"]);
+    expect(arrays.right[2]).toBe(5);
   })
-  test("it is false", () => { 
-    expect(false).toBe(true);
-  })
-})
+}),
 
-describe("Say hello", () => {
-  test("it says hello world", () => {
-    let res = day.SayHelloWorld();
-    expect(res).toBe("Hello World!");
+describe("sort arrays", () => {
+  test("sorted left array first element is 1", () => {
+    let array = day.SortList([3,4,2,1,3,3]);
+    expect(array[0]).toBe(1);
+  }),
+  test("sorted right array first element is 3", () => {
+    let array = day.SortList([4,3,5,3,9,3]);
+    expect(array[0]).toBe(3);
+  })
+}),
+
+describe("pair distances", () => {
+  test("distance between 1 & 3 is 2",() => {
+    let distance = day.PairDistance(1, 3);
+    expect(distance).toBe(2);
+  })
+}),
+
+describe("pair distance sums", () => {
+  test("sum of pair distances is 11", () => {
+    let sum = day.DistanceSums(["3   4","4   3","2   5","1   3","3   9","3   3"]);
+    expect(sum).toBe(11);
   })
 })
